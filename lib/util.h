bcf3e6c2421849548fab9f7122d6415b4356fa0c
OzPineapple <zush18@outlook.com>
2021-12-08 20:39:18 -0600

    rename include dir

---
+++ lib/util.h
@@ -0,0 +1,14 @@
+/* Source code from Suckless.com */
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdlib.h>
+
+#define MAX(A, B)               ((A) > (B) ? (A) : (B))
+#define MIN(A, B)               ((A) < (B) ? (A) : (B))
+#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
+
+void die(const char *fmt, ...);
+void *ecalloc(size_t nmemb, size_t size);
+
+#endif // UTIL_H
---
+++ lib/wav.h
@@ -0,0 +1,47 @@
+/* Autor Oz */
+#ifndef WAV_H
+#define WAV_H
+
+typedef struct {
+	char			id[4];		// RIFF
+	unsigned int	size;		// File Size
+	char			format[4];	// WAV
+} Riff;
+
+typedef struct {
+	char			id[4];
+	unsigned int	size;
+	unsigned short	audiofmt;
+	unsigned short	channels;
+	unsigned int	samplerate;
+	unsigned int	byterate;
+	unsigned short	blockalign;
+	unsigned short	bitspersample;
+} Fmt;
+
+typedef struct {
+	char			id[4];
+	unsigned int	size;
+	char			*data;
+} List;
+
+typedef struct {
+	char			id[4];
+	unsigned int	size;
+	char			*data;
+} Data;
+
+typedef struct {
+	Riff	*riff;
+	Fmt		*fmt;
+	List	*list;
+	Data	*data;
+} Wav;
+
+//SIGNATURES
+Wav *initWav( char * file );
+void writeWav( Wav *wav, char *file );
+Wav *destroyWav( Wav * wav );
+void delNegWav( Wav *wav );
+void infoWav( Wav *wav );
+#endif //_INCLUDE/WAV_H
