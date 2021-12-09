51a0e5ec6f33a80d5301f37b44688da7edc21c05
OzPineapple <zush18@outlook.com>
2021-12-08 20:38:31 -0600

    Fix include

--- vol.c
+++ vol.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "include/wav.h"
-#include "include/util.h"
+#include "lib/wav.h"
+#include "lib/util.h"
 
 int main(int argc, char ** argv ){
 	if( argc < 3 ) die("Faltan argumentos: in.wav out.wav");
 	Wav* wav = initWav( argv[1] );
 	//infoWav( wav );
-	for( size_t i=0; i<( wav->data->size & 0xEFFF ); i+=2)
+	for( size_t i=0; i <( wav->data->size >> 1 ); i+=2)
 		*(short*)( wav->data->data + i) =
 		*(short*)( wav->data->data + i) >> 1;
 	writeWav( wav, argv[2] );
